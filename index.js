509a411ee9c6353ee5ac9d01aece199b1e2d896a
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-11-02 23:52:28 +0600

    project complete with nodemon and delete section error

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1ndgjy2.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('geniusCar').collection('services');
+        const orderCollection = client.db('geniusCar').collection('orders');
+        // console.log('connect');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            console.log(services);
+            res.send(services)
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service);
+        });
+
+        // orders api
+
+        app.get('/orders', async (req, res) => {
+            console.log(req.query);
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query)
+            const order = await cursor.toArray()
+            res.send(order);
+        })
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await orderCollection.updatedDoc(query, updatedDoc)
+            res.send(result)
+        })
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
+
+
+
+app.get('/', (req, res) => {
+    res.send('genius car server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Genius Car Server is running on ${port}`);
+})
